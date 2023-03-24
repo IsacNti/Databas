062fcdb9d5faef3479b99beeab63ef6647c2aa7d
IsacNti <77719323+IsacNti@users.noreply.github.com>
2023-03-24 13:37:48 +0100

    Initial commit

---
+++ src/DB.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+import java.sql.*;
+
+
+public class DB {
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        JPasswordField pf = new JPasswordField();
+
+
+        // Set up connection to database
+        try {
+            /*conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bookshop"+
+                            "? allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
+                    user,password);
+*/
+            conn = DriverManager.getConnection("jdbc:mysql://" + env.DBURL + ":" + env.port + "/" + env.DBname +
+                            "? allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
+                    env.user, env.password);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            Statement stmt = conn.createStatement();
+            String SQLQuery = env.DBtable;
+            ResultSet result = stmt.executeQuery(SQLQuery);
+
+            ResultSetMetaData metadata = result.getMetaData();
+
+            int numCols = metadata.getColumnCount();
+            for (int i = 1 ; i <= numCols ; i++) {
+                System.out.println(metadata.getColumnClassName(i));
+            }
+
+            while (result.next()) {
+                String output = "";
+                output += result.getInt("id") + ", " +
+                        result.getString("createdAt") + ", " +
+                        result.getString("title") + ", " +
+                        result.getString("content") + ", " +
+                        result.getInt("authorId");
+                System.out.println(output);
+            }
+
+            stmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
